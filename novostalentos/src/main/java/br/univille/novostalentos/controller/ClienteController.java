b1546da143264fc5673d4d74a20cfe125d4b6f1f
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-09-29 14:34:55 -0300

    WIP: integração com JPA e banco de dados

--- novostalentos/src/main/java/br/univille/novostalentos/controller/ClienteController.java
+++ novostalentos/src/main/java/br/univille/novostalentos/controller/ClienteController.java
@@ -1,10 +1,10 @@
 package br.univille.novostalentos.controller;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import br.univille.novostalentos.entity.Cliente;
 
 @Controller  
@@ -13,14 +13,31 @@ public class ClienteController {
 
       @GetMapping
       public ModelAndView index(){
-          Cliente cliente1 = new Cliente();
-          cliente1.setEndereco("Rua lalala 100");
-          cliente1.setId(1);
-          cliente1.setNome("Zezinho");
-          cliente1.setSexo("Masculino");
+         
+        ArrayList<Cliente> listaClientes = new ArrayList<>();
 
-          
-        return new ModelAndView("cliente/index");
+        var cliente1 = new Cliente();
+        cliente1.setEndereco("Rua lalala 100");
+        cliente1.setId(1);
+        cliente1.setNome("Zezinho");
+        cliente1.setSexo("Masculino");
+        listaClientes.add(cliente1);
+
+        var cliente2 = new Cliente();
+        cliente2.setEndereco("Rua lelele 200");
+        cliente2.setId(2);
+        cliente2.setNome("Luiznho");
+        cliente2.setSexo("Masculino");
+        listaClientes.add(cliente2);
+
+        var cliente3 = new Cliente();
+        cliente3.setEndereco("Rua lululu 300");
+        cliente3.setId(3);
+        cliente3.setNome("Huguinho");
+        cliente3.setSexo("Masculino");
+        listaClientes.add(cliente3);
+  
+        return new ModelAndView("cliente/index","listaClientes",listaClientes);
       }
     
 }
