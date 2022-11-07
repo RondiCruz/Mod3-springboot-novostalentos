1f9290bea55e23d747a1c3e68a6b601793595034
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-11-07 13:38:08 -0300

    Implementação do Spring Security

---
+++ novostalentos/src/main/java/br/univille/novostalentos/Startup.java
@@ -0,0 +1,24 @@
+package br.univille.novostalentos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+
+import br.univille.novostalentos.entity.Usuario;
+import br.univille.novostalentos.entity.Usuario;
+import br.univille.novostalentos.repository.UsuarioRepository;
+
+public class Startup {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event){
+        if(repository.findByNome("admin") ==  null){
+            var adminUser = new Usuario();
+            adminUser.setNome("admin");
+            adminUser.setSenha("admin");
+            repository.save(adminUser);
+        }
+    }
+}
