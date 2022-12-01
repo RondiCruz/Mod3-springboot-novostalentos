d519738a434bed70cc814e4cbfafeae763b2aaab
RONDINELI DE LIMA CRUZ <rondineli.cruz@univille.br>
2022-12-01 14:33:52 -0300

    Atualização do código

--- novostalentos/src/main/java/br/univille/novostalentos/Startup.java
+++ novostalentos/src/main/java/br/univille/novostalentos/Startup.java
@@ -3,11 +3,13 @@ package br.univille.novostalentos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 import br.univille.novostalentos.entity.Usuario;
 import br.univille.novostalentos.entity.Usuario;
 import br.univille.novostalentos.repository.UsuarioRepository;
 
+@Component
 public class Startup {
     @Autowired
     private UsuarioRepository repository;
